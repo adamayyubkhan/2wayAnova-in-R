676ff3aa7a42e59d58d7e3ea1bdde12a9a6bdc81
Ayyub <68425252+adamayyubkhan@users.noreply.github.com>
2022-11-23 15:21:39 +0000

    Update 2way anova Pax7.R

--- 2way anova Pax7.R
+++ 2way anova Pax7.R
@@ -14,25 +14,34 @@ library(tidyverse)
 
 head(zebrafish)
 str(zebrafish)
+#I first wanted to check the first 6 and bottom 6 of the data set to make sure that al factors were correctly labelled prior to any statiscial analys
 
 zebrafish$Treatment <- as.factor(zebrafish$Treatment)
 zebrafish$drug <- as.factor(zebrafish$drug)
+#The factors were not correctly labelled hence I coded for these adjustments
 
 str(zebrafish)
 head(zebrafish)
+#I checked to make sure the necessary changes had occured as planned
+
 
 ggplot(data = zebrafish, mapping = aes(Treatment, pax7_expression, fill = drug))+
   geom_boxplot()+
   theme_classic()
+#I plotted the iniital data set to review the data
 
 twoANOVA <- aov(pax7_expression ~ factor(Treatment)* factor(drug), data = zebrafish)
 summary(twoANOVA)
+#This provided a statitcal table summary of the twoANOVA for me to 
 
 TUKEY <- TukeyHSD(x=twoANOVA, conf.level = .95)
 plot(TUKEY, las=1 , col='blue')
+#The initial plot function in R produced a graph which cuts off the y axis labels.
+#In order to work around this I had to use the ggplot function in order to create a way around this
 
 df <- TUKEY[["factor(Treatment):factor(drug)"]] %>% as.data.frame() 
 df2 <- tibble::rownames_to_column(df, "id")
+#I had to name the inital column with all the y axis titels as id
 df2%>%
   ggplot(aes(id))+
   geom_errorbar(aes(ymin=lwr,
@@ -44,7 +53,7 @@ df2%>%
              color="red")+
   labs(x=NULL)+
   coord_flip()
-
+#The graph had to be designed inverted in order to allow the cord_flip function to then rever the graph to the correct orientation.
 
 
 
